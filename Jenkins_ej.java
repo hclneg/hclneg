5fec1983ffe9db8947e8515d64f932dbbb66e317
Heidi López <heidicarolina@gmail.com>
2023-09-26 18:32:01 -0500

    Subiendo archivo Jenking_ej

---
+++ Jenkins_ej.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jenkins_ej;
+
+/**
+ *
+ * @author 57311
+ */
+public class Jenkins_ej {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world by Heidi");
+    }
+    
+}
